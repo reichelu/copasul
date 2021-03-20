2d190e8f59d6b53fa04947e304361d3d101d5f54
reichelu <reichelu@phonetik.uni-muenchen.de>
2021-03-20 14:50:28 +0100

    example_call.py added

---
+++ src/example_call.py
@@ -0,0 +1,18 @@
+import os
+import json
+import copasul
+
+# load minimal example config
+pth = os.path.dirname(os.path.abspath(__file__))
+with open("{}/../minex/config/minex.json".format(pth), "r") as h:
+    opt = json.load(h)
+
+# init Copasul() object
+fex = copasul.Copasul()
+
+# process data specified in config
+copa = fex.process(config=opt)
+
+# access output, e.g. local contour feature set
+print("local contour features:")
+print(copa["export"]["loc"])
